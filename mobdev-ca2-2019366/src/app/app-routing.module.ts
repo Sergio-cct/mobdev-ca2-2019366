46158f6a6dd40456ef191ff1f784c94fb24307ac
Sergio-cct <sergio.silva1309@gmail.com>
2020-04-22 08:49:45 +0000

    Version 4

--- mobdev-ca2-2019366/src/app/app-routing.module.ts
+++ mobdev-ca2-2019366/src/app/app-routing.module.ts
@@ -3,22 +3,34 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-   {
-    path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'character-details',
-    loadChildren: () => import('./pages/character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
-  }
-  ];
+    {
+        path: '', loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    },
+
+    {
+        path: 'episodes',
+        loadChildren: () => import('./pages/episodes/episodes.module').then(m => m.EpisodesPageModule)
+    },
+    {
+        path: 'episode-details',
+        loadChildren: () => import('./pages/episode-details/episode-details.module').then(m => m.EpisodeDetailsPageModule)
+    },
+  
+  
+    {
+        path: 'characters',
+        loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersPageModule)
+    },
+    {
+        path: 'character-details',
+        loadChildren: () => import('./pages/character-details/character-details.module').then(m => m.CharacterDetailsPageModule)
+    }
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
